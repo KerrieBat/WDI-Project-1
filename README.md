18c56e0bb14be1e82cd2cfe6102befb2d02532cc
Kerrie Batrouney <batrouney@gmail.com>
2016-10-13 22:40:04 +1100

    version 2, added scores and flashing win line

--- README.md
+++ README.md
@@ -1,36 +1,27 @@
-# ![](https://ga-dash.s3.amazonaws.com/production/assets/logo-9f88ae6c9c3871690e33280fcf557f33.png) Project #1: The Game
+# ![](https://ga-dash.s3.amazonaws.com/production/assets/logo-9f88ae6c9c3871690e33280fcf557f33.png) Project #1: TIC TAC TOE
 
-### Overview
-
-Let's start out with something fun - **a game!**
-
-Everyone will get a chance to **be creative**, and work through some really **tough programming challenges** – Let's start out with something fun - a game!  It's up to you to come up with a fun and interesting game to build. I recommend tic tac toe (knots and crosses)
-
-**You will be working individually for this project**, but we'll be guiding you along the process and helping as you go. Show us what you've got!
-
+This game has been written as first project in the General ASsembly Web Development Immersive course, by KerrieBat.
 
 ---
 
-### Technical Requirements
+### Technologies Used
+It's a **fun** game in Javascript, jQuery, html, css.
+### Approach taken
+Javascript file is heavily commented.
+I tried to make many small functions and declared all variables at the top.
+Once the basic game was working, other features were added. These include keeping score for multiple games, adding the message bar and flashing the winning line.
+The squares on the board have a representing array. There is an array of arrays to represent the winning combinations of squares. There are several toggles to keep track of progress, gameWon, gameDraw, turn. The message box contents changes with instructions, prompts and announcing the winner. The layout design is responsive and fabulous.
 
-Your app must:
-
-* **Render a game in the browser**
-* **Switch turns** between two players
-* **Design logic for winning** & **visually display which player won**
-* **Include separate HTML / CSS / JavaScript files**
-* Stick with **KISS (Keep It Simple Stupid)** and **DRY (Don't Repeat Yourself)** principles
-* Use **Javascript or jQuery** for **DOM manipulation**
-* **Deploy your game online**, where the rest of the world can access it
-* Use **semantic markup** for HTML and CSS (adhere to best practices)
+Testing revealed more opportunities to introduce more variables and if statements!
 
 ---
 
-### Necessary Deliverables
+### Installation Instruction
+None!
 
-* A **working game, built by you**, hosted somewhere on the internet
-* A **link to your hosted working game** in the URL section of your GitHub repo
-* A **git repository hosted on GitHub**, with a link to your hosted game, and frequent commits dating back to the very beginning of the project
-* **A ``readme.md`` file** with explanations of the technologies used, the approach taken, installation instructions, unsolved problems, etc.
+---
+
+### Unsolved Problems / extensions
+I could add more error traps, if you do not click start game, just a square directly, player does not switch, also after the game is won, it's possible to keep adding icons to the board'. I'd like to add a monster picker
 
 ---
--- scripts/game.js
+++ scripts/game.js
@@ -1,11 +1,8 @@
 
-
+var instr = "This is a simple game of tic tac toe for two players. First type in your names and click on NEW GAME, then take it in turns to click in the square you choose. If you press PLAY AGAIN, this will keep a running tally of the scores. NEW GAME will reset the scores";
 //set up variables
 console.log('hello');
-// array of squares
-//var rows=[];//array of a row
-var squares=[];//array of all rows
-//var arraySize = 3;//a square array only
+var squares=[];// array of squares
 var $player1Input = $('#p1');
 var $player2Input = $('#p2');
 var imageX = "http://iconbug.com/data/b5/256/390e2c432ea26d61fc6f72ba8f148894.png"; //cross
@@ -18,14 +15,23 @@ var $clickOnBoard = $('.squares');
 var turn = true;//keeps record of whose turn it is
 var $messageBox = $('#message');//box for instructions and winner announcement
 var gameWon = false;
+var gameDraw = false;
 var scoreOfP1 = 0;
 var scoreOfP2 = 0;
-
+var $scoreOfP1 = $('#scoreP1');
+var $scoreOfP2 = $('#scoreP2');
+var win1;
+var win2;
+var win3;
 
 var winningArray =[[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]] //all possible connotations of a win
 
-// makeEmptyArray() and clear board
+
 var makeEmptyArray = function() {
+  clearInterval(win1);
+  clearInterval(win2);
+  clearInterval(win3);
+// makeEmptyArray() and clear board
   squares =[];
   for (i=0; i<9; i++) {
     squares.push("");
@@ -37,23 +43,21 @@ var makeEmptyArray = function() {
 }
 }
 
+
 var startGame = function(event){
   gameWon=false;
-  //debugger
+  gameDraw = false;
+  $('.board img').show()
   makeEmptyArray();
-  //getPlayers();
   $messageBox.text("OK " + $player1Input.val() + " click on a square");
-
-// player 1....
-// var turn = true;
-// return
 }
+
 
 var makePlay = function(event){
 //check who's turn it is
   if (turn === true) {
     // add eventlistener on the board and insert 0 into array
-   //keeps record of whose turn it is
+    //keeps record of whose turn it is
     //use event target to equate position on board to position in array
     var $posnClicked = Number(event.target.id);
     console.log($posnClicked);
@@ -61,11 +65,9 @@ var makePlay = function(event){
     squares[$posnClicked] = "0";
     //display eye onto squares
     // div id is $posnClicked, img id is img$posnClicked
-
     var squareId = "#img" + $posnClicked;
      var $imgId = $(squareId);
     $imgId.attr('src', image0);
-
   } else {
     //as above but for 'X'
     var $posnClicked = Number(event.target.id);
@@ -74,7 +76,6 @@ var makePlay = function(event){
     var squareId = "#img" + $posnClicked;
     var $imgId = $(squareId);
     $imgId.attr('src', imageX);
-
   }
     checkWinningMove(turn);
     if (gameWon === true) {
@@ -82,13 +83,8 @@ var makePlay = function(event){
     }
 }
 
-// var finishGame = function{
-//
-// }
 
 var checkWinningMove = function(){
-//  debugger
-//console.log('sdfsfsdfsf');
 //Check to see if this move won the game
   if (turn === true) {
   //check for 0s in array
@@ -98,6 +94,17 @@ var checkWinningMove = function(){
       var winningCombination = [winningArray[i]];
       if ((squares[winningArray[i][0]] === "0") && (squares[winningArray[i][1]] ==="0") && (squares[winningArray[i][2]])==="0") {
         // then winning move
+        //flash winning line
+        //position of winning squares winningArray[i][0]and [1] and [2]
+        //images which need to flash are $('#img + winningArray[i][0]')
+        var $winningImage1 = $('#img'+winningArray[i][0]);
+        var $winningImage2 = $('#img'+winningArray[i][1]);
+        var $winningImage3 = $('#img'+winningArray[i][2]);
+
+        win1 = setInterval(function() {$winningImage1.toggle();}, 300);
+        win2 = setInterval(function() {$winningImage2.toggle();}, 300);
+        win3 = setInterval(function() {$winningImage3.toggle();}, 300);
+
         winnerOfGame(turn);
         return gameWon;
       }
@@ -107,6 +114,20 @@ var checkWinningMove = function(){
     for (i=0; i<winningArray.length; i++) {
     var winningCombination = [winningArray[i]];
     if ((squares[winningArray[i][0]] === "X") && (squares[winningArray[i][1]] ==="X") && (squares[winningArray[i][2]])==="X") {
+      //flash winning line
+      var $winningImage1 = $('#img'+winningArray[i][0]);
+      var $winningImage2 = $('#img'+winningArray[i][1]);
+      var $winningImage3 = $('#img'+winningArray[i][2]);
+
+      win1 = setInterval(function() {
+        $winningImage1.toggle();
+      }, 300);
+      win2 = setInterval(function() {
+        $winningImage2.toggle();
+      }, 300);
+      win3 = setInterval(function() {
+        $winningImage3.toggle();
+      }, 300);
       // then winning move
       winnerOfGame(turn);
       return gameWon;
@@ -118,8 +139,9 @@ var checkWinningMove = function(){
   } else {
 // if not winning move.... is board full?
 if (squares.indexOf("") === -1){
-    $messageBox.text("No winner this time, to play again, click on PLAY");
+    $messageBox.text("No winner this time, click on PLAY AGAIN");
     gameWon = true;
+    gameDraw = true;
     return gameWon;
   }
 //if not winning move, toggle turn!!!!
@@ -136,45 +158,64 @@ return turn;
 }
 
 var winnerOfGame = function(){
-//  debugger
   //based on whos turn it is the winner is announced
   //with bells and whistles
-console.log("there's a winner");
-
-if (turn === true) {
-  //winner is player 1
-  console.log("winner is Player 1");
-  $messageBox.text("WINNER is " + $player1Input.val() + "....... To play again, press PLAY AGAIN");
-} else {
-  //winner is player 2
-  console.log("winner is Player 2");
-  $messageBox.text("WINNER is " + $player2Input.val() + "....... To play again, press PLAY AGAIN");
+  console.log("there's a winner");
+  if (turn === true) {
+    //winner is player 1
+    console.log("winner is Player 1");
+    $messageBox.text("WINNER is " + $player1Input.val() + "....... To play again, press PLAY AGAIN");
+  } else {
+    //winner is player 2
+    console.log("winner is Player 2");
+    $messageBox.text("WINNER is " + $player2Input.val() + "....... To play again, press PLAY AGAIN");
+  }
+  //debugger
+  //there is a winner, so we need to finish the game
+  gameWon = true;
+  return gameWon;
 }
-//debugger
-//there is a winner, so we need to finish the game
-gameWon = true;
-return gameWon;
-}
 
-// var nextGo = function(){
-// // to  switch player
-// //change player background-color and send message your go();
-// console.log("your go");
-// }
 
 var keepScore = function(){
 //add 1 to score of winning players
+  if (gameWon === true && gameDraw===false) {
+    if (turn === true) {
+      // debugger
+      scoreOfP1 ++;
+      $scoreOfP1.val(scoreOfP1);
+    } else {
+      scoreOfP2 ++;
+      $scoreOfP2.val(scoreOfP2);
 
-startGame();
+    }
+}
+  startGame();
+}
 
+
+var resetScore = function(){
+  scoreOfP2 = 0;
+  scoreOfP1 = 0;
+  $scoreOfP1.val(scoreOfP1);
+  $scoreOfP2.val(scoreOfP2);
+  startGame();
 }
 
 var instructions = function(){
-  alert("contents of readme file");
+  alert(instr);
 }
 
-$playBtn.click(startGame);
+var finishGame = function() {
+  scoreOfP2 = 0;
+  scoreOfP1 = 0;
+  $scoreOfP1.val(scoreOfP1);
+  $scoreOfP2.val(scoreOfP2);
+  makeEmptyArray();
+}
+
+$playBtn.click(resetScore);
 $againBtn.click(keepScore);
 $clickOnBoard.click(makePlay);
-//$finishBtn.click(do something);
+$finishBtn.click(finishGame);
 $instructionsBtn.click(instructions);
